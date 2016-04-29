5b54803bde5659a9f6ef15d464641d745c232897
John Vandenberg <jayvdb@gmail.com>
2016-04-29 23:56:55 +0700

    Remove doctests

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ pip freeze
 cd tests/test_plugin
 python setup.py install
 cd ../../
-nosetests --with-doctest --doctest-extension=.rst doc/source pyexcel tests README.rst
+nosetests tests
 if [ $? == 0 ] ; then
 	rm tmp.db
 else
